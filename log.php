ccac58c63b4a086c8540b4c872c3e03f89d797b7
lapislazuli0929 <100329159+lapislazuli0929@users.noreply.github.com>
2022-03-23 21:44:32 +0800

    hw file

---
+++ log.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <center><font size="10">登入</font></center>
+</head>
+
+<body>
+    <form action="check.php" method="post"><br>
+    <input type="text" name="account" placeholder="輸入帳號" required><br>
+    <input type="password  " name="password" placeholder="輸入密碼" required><br>
+    <input type="submit" value="Login">
+    </form>
+</body>
+</html>
